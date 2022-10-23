81c66fda195f0b8d49c5709b6956385a9e0985d0
Garinyr <garinyr92@gmail.com>
2022-10-23 13:25:50 +0700

    update readme & simple open url feature

--- src/test/java/steps/CommonSteps.java
+++ src/test/java/steps/CommonSteps.java
@@ -2,29 +2,37 @@ package steps;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
-import org.junit.Assert;
+import io.cucumber.java.en.When;
+import org.testng.Assert;
 import org.openqa.selenium.WebDriver;
+import pageobject.HomePO;
 import utilities.SeleniumHelpers;
 import utilities.ThreadManager;
 
 public class CommonSteps {
     private WebDriver driver = ThreadManager.getDriver();
     private SeleniumHelpers selenium = new SeleniumHelpers(driver);
+    private HomePO home = new HomePO(driver);
 
     @Given("user access page {string}")
     public void user_access_page(String url) {
         selenium.navigateToPage(url);
     }
 
-    @Given("user access page hdi")
-    public void user_access_page_hdi() {
-        selenium.navigateToPage("https://www.hdistore.com");
+    @Given("user access page automationpractice")
+    public void user_access_page_automation_practice() {
+        selenium.navigateToPage("https://rahulshettyacademy.com/AutomationPractice/");
     }
 
-
+    @When("users on {string}")
+    public void usersOnWebsite(String title) {
+        Assert.assertEquals(title, home.getTitle());
+    }
 
     @Then("compare current url with {string}")
     public void compare_url(String url){
         Assert.assertEquals(url, selenium.getURL());
    }
+
+
 }
